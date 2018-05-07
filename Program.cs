69f2a8c00844f5ff0e18f05889651108f25f264c
burkaans <raimonds.buholts@gmail.com>
2018-05-07 20:05:51 +0300

    day2_1
    day_2/Projekta_izveide

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day_2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Labdien, ievadiet savu dzimšanas gadu");
+            String gads;
+            gads = Console.ReadLine();
+            Console.WriteLine("kurā mēnesī esat dzimis?");
+            String menesis;
+            menesis = Console.ReadLine();
+            Console.WriteLine("un visbeidzot, dzimšanas datums");
+            String diena;
+            diena = Console.ReadLine();
+            Console.WriteLine("Jūs esat dzimis " + gads + ". gada " + diena + ". " + menesis + "! Visu labu!");
+           
+
+            Console.ReadLine();
+
+        }
+    }
+}
